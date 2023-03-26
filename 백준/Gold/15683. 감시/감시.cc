bd8230c2a8ac88b57e2f309c848ff7a1a642df02
seyi103 <74170226+seyi103@users.noreply.github.com>
2023-03-26 22:02:24 +0900

    Update 감시.cc

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,97 +1,116 @@
+//#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//#include <iostream>
 #include <stdio.h>
 #include <vector>
 
 using namespace std;
 
 int N, M;
-int MAT[9][9];
+int MAP[9][9];
 
-vector <pair<int, int>> vec;
+vector <pair<int, int>> cctv;
 
-int dr[] = { -1,0,1,0 };
-int dc[] = { 0,-1,0,1 };
+int ydir[] = { -1,0,1,0 };
+int xdir[] = { 0,-1,0,1 };
 
-int ans;
+int ans = 21e8;
 
-void monitoring(int r, int c, int d, int org, int chg) { // 감시 지역 마킹
-	while (true) {
-		r = r + dr[d]; c = c + dc[d];
-		if (r < 0 || r >= N || c < 0 || c >= M || MAT[r][c] == 6) {
+// 감시 영역을 채울건데
+// ch로 감시 중인 영역을 채울거임
+void checkArea(int y, int x, int d, int em, int ch) { 
+	while (true) 
+	{
+		y += ydir[d]; 
+		x += xdir[d];
+		if (y < 0 || y >= N || x < 0 || x >= M || MAP[y][x] == 6) 
 			break;
-		}
-		if (MAT[r][c] == org) {
-			MAT[r][c] = chg;
-		}
+		
+		if (MAP[y][x] == em) 
+			MAP[y][x] = ch;
 	}
 }
 
-void opt(int r, int c, int d, int org, int chg) { // CCTV가 감시할 지역 마킹
-	if (MAT[r][c] == 1) { // 1번 CCTV인 경우
-		monitoring(r, c, d, org, chg);
+void cctvtype(int y, int x, int d, int em, int ch) { 
+	// 1
+	if (MAP[y][x] == 1) { 
+		checkArea(y, x, d, em, ch);
 	}
-	else if (MAT[r][c] == 2) { // 2번 CCTV인 경우
-		monitoring(r, c, d, org, chg);
-		monitoring(r, c, (d + 2) % 4, org, chg);
+	// 2
+	else if (MAP[y][x] == 2) { 
+		checkArea(y, x, d, em, ch);
+		checkArea(y, x, (d + 2) % 4, em, ch);
 	}
-	else if (MAT[r][c] == 3) { // 3번 CCTV인 경우
-		monitoring(r, c, d, org, chg);
-		monitoring(r, c, (d + 1) % 4, org, chg);
+	// 3
+	else if (MAP[y][x] == 3) { 
+		checkArea(y, x, d, em, ch);
+		checkArea(y, x, (d + 1) % 4, em, ch);
 	}
-	else if (MAT[r][c] == 4) { // 4번 CCTV인 경우
-		monitoring(r, c, d, org, chg);
-		monitoring(r, c, (d + 1) % 4, org, chg);
-		monitoring(r, c, (d + 3) % 4, org, chg);
+	// 4
+	else if (MAP[y][x] == 4) { 
+		checkArea(y, x, d, em, ch);
+		checkArea(y, x, (d + 1) % 4, em, ch);
+		checkArea(y, x, (d + 3) % 4, em, ch);
 	}
 }
 
-void dfs(int num){ //감시 지역 탐색!
-	if (num == vec.size()) {
-		int cnt = 0;
+void dfs(int now) { 
+	if (now == cctv.size()) {
+		int area = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (MAT[i][j] == 0) {
-					cnt++;
-				}
+				if (MAP[i][j] == 0) 
+					area++;
 			}
 		}
-		if (ans > cnt) {
-			ans = cnt;
-		}
+		if (ans > area) 
+			ans = area;
 	}
 	else {
-		int r = vec[num].first; int c = vec[num].second;
+		int y = cctv[now].first; 
+		int x = cctv[now].second;
+
 		for (int i = 0; i < 4; i++) {
-			opt(r, c, i, 0, -(num + 1)); // n 번째 CCTV가 감시할 지역을 -(n+1)로 마킹
-			dfs(num + 1);
-			opt(r, c, i, -(num + 1), 0); //원상복귀
+			// 내가 현재 now번째 cctv를 확인 중이면
+			// 감시 영역은 -(now + 1)로 채워두자
+			cctvtype(y, x, i, 0, -(now + 1)); 
+			dfs(now + 1);
+			// 복구
+			cctvtype(y, x, i, -(now + 1), 0); 
 		}
-		//dfs(num + 1);
-		
 	}
 }
 
 int main() {
+	//fastIO;
+
+	//cin >> N >> M;
 	scanf("%d %d", &N, &M);
-	vec.clear();
+	cctv.clear();
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			scanf("%d", &MAT[i][j]);
-			if (MAT[i][j] >= 1 && MAT[i][j] <= 5) {
-				vec.push_back(make_pair(i, j)); // CCTV 위치 저장
+			//cin >> MAP[i][j];
+			scanf("%d", &MAP[i][j]);
+			if (MAP[i][j] >= 1 && MAP[i][j] <= 5) {
+				cctv.push_back({ i, j }); // CCTV 위치 저장
 			}
 		}
 	}
-	int num_cctv = vec.size();
-	for (int i = num_cctv - 1; i >= 0; i--) { //5번 타입 CCTV 유무 확인
-		int x = vec[i].first; int y = vec[i].second;
-		if (MAT[x][y] == 5) { //5번 타입의 CCTV 라면 감시지역을 -10으로 체크 
-			for (int n = 0; n < 4; n++) {
-				monitoring(x, y, n, 0, -10);
-			}
-			vec.erase(vec.begin() + i); //5번 타입 CCTV 위치 삭제
+
+	for (int i = cctv.size() - 1; i >= 0; i--) {
+		int y = cctv[i].first; 
+		int x = cctv[i].second;
+		// 5번 타입은 4방향 다보는 경우 1가지
+		if (MAP[y][x] == 5) { 
+			for (int n = 0; n < 4; n++) 
+				// 영향을 주지 않는 값으로 채워 두자
+				checkArea(y, x, n, 0, -10);
+			cctv.erase(cctv.begin() + i); 
 		}
 	}
-	ans = 64;
-	dfs(0); // 감시지역 탐색
+
+	dfs(0); 
+
+	//cout << ans;
 	printf("%d", ans);
+	return 0;
 }
