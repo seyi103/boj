e5dcf13276c02bb7251946d0f30059b8a36b0ed4
seyi103 <74170226+seyi103@users.noreply.github.com>
2023-05-21 21:39:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 6 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.cpp
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.cpp
@@ -1,90 +1,88 @@
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <queue>
+#include <cstring>
 using namespace std;
 
 struct Honey {
-    int cost;
-    int y;
-    int sx;
-    int ex;
+	int y, sx, ex;
+	int cost;
 };
 
-int N, M; // 맵크기, 선택가능한 개수
+int N, M, C;
 int MAP[11][11];
-int C; // 채취 가능한 최대 양
+vector<Honey> h;
 int tmp;
-vector<Honey> h; // 채취 가능한 최대 꿀양들을 담아 놓을 vector
 
-void INIT() {
-    memset(MAP, 0, sizeof(MAP));
-    h.clear();
+void init() {
+	memset(MAP, 0, sizeof(MAP));
+	h.clear();
 }
 
 void input() {
-    cin >> N >> M >> C;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            cin >> MAP[i][j];
+	cin >> N >> M >> C;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> MAP[i][j];
+		}
+	}
 }
 
-void dfs(int y, int sx, int ex, int sum, int now) {
-    // 채취하려는 합이 C보다 크면 못함
-    if (now > C)
-        return;
+void dfs(int y, int x, int dx, int sum, int now) {
+	// 가지치기
+	if (now > C)
+		return;
 
-    if (sx == ex) {
-        tmp = max(tmp, sum);
-        return;
-    }
-
-    // 꿀을 채취
-    dfs(y, sx + 1, ex, sum + MAP[y][sx] * MAP[y][sx], now + MAP[y][sx]);
-    // 채취하지 않음
-    dfs(y, sx + 1, ex, sum, now);
-}
-
-void getHoney() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j <= N - M; j++) {
-            tmp = 0;
-            dfs(i, j, j + M, 0, 0);
-            h.push_back({ tmp, i, j, j + M - 1 });
-        }
-    }
-}
-
-void findMax(int& res) {
-    for (int i = 0; i < h.size(); i++) {
-        for (int j = i + 1; j < h.size(); j++) {
-            Honey A = h[i], B = h[j];
-            if (A.y == B.y && (A.ex <= B.sx || A.sx <= B.ex))
-                continue;
-            res = max(res, A.cost + B.cost);
-        }
-    }
+	// 재귀 구성
+	if (x == dx) {
+		if (tmp < sum)
+			tmp = sum;
+		return;
+	}
+	// 재취한다
+	dfs(y, x + 1, dx, sum + MAP[y][x] * MAP[y][x], now + MAP[y][x]);
+	// 안한다
+	dfs(y, x + 1, dx, sum, now);
 }
 
 int solve() {
-    int res = 0;
-    getHoney();
-    findMax(res);
-    return res;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= N - M; j++) {
+			tmp = 0;
+			dfs(i, j, j + M, 0, 0);
+			h.push_back({ i, j, j + M - 1, tmp });
+		}
+	}
+
+	int res = -21e8;
+	int ed = h.size();
+	for (int i = 0; i < ed; i++) {
+		for (int j = i + 1; j < ed; j++) {
+			Honey A = h[i], B = h[j];
+			if (A.y == B.y && (A.ex <= B.sx || A.sx <= B.ex))
+				continue;
+			int sum = A.cost + B.cost;
+			if (sum > res)
+				res = sum;
+		}
+	}
+	return res;
 }
 
 int main() {
-    int T;
-    cin >> T;
-    for (int tc = 1; tc <= T; tc++) {
-        // reset
-        INIT();
-        // input
-        input();
-        // solve
-        int ans = solve();
-        // output
-        cout << '#' << tc << ' ' << ans << '\n';
-    }
-    return 0;
+	fastIO;
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		// reset
+		init();
+		// input
+		input();
+		// solve
+		int ans = solve();
+		// output
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
+
+	return 0;
 }
