5973366a4f0ffa8db8572bb26be98f28898a1afa
seyi103 <74170226+seyi103@users.noreply.github.com>
2023-06-24 10:51:38 +0900

    [D3] Title: 문자열 교집합, Time: 2,252 ms, Memory: 29,552 KB -BaekjoonHub

---
+++ SWEA/D3/2948. 문자열 교집합/README.md
@@ -0,0 +1,11 @@
+# [D3] 문자열 교집합 - 2948 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV-Un3G64SUDFAXr) 
+
+### 성능 요약
+
+메모리: 29,552 KB, 시간: 2,252 ms, 코드길이: 704 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.cpp
@@ -0,0 +1,40 @@
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main() {
+	fastIO;
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		// reset
+		int N, M;
+		unordered_map<string, int> um;
+		int cnt = 0;
+		// input
+		cin >> N >> M;
+		for (int i = 0; i < N; i++) {
+			string str;
+			cin >> str;
+			if (um.find(str) != um.end())
+				um[str]++, cnt++;
+			else
+				um[str] = 1;
+		}
+		for (int i = 0; i < M; i++) {
+			string str;
+			cin >> str;
+			if (um.find(str) != um.end())
+				um[str]++, cnt++;
+			else
+				um[str] = 1;
+		}
+		// solve
+
+		// output
+		cout << '#' << tc << ' ' << cnt << '\n';
+	}
+	return 0;
+}
